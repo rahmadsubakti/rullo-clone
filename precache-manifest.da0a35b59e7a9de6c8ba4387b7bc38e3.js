b0772a0591d28eeb19524e6b724f37bd6c6e00af
Rahmad Subakti <rahmadsubakti@github.com>
2020-03-16 06:53:49 +0800

    Updates

---
+++ precache-manifest.da0a35b59e7a9de6c8ba4387b7bc38e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0a4875e346606c19d77559c2c8f23e",
+    "url": "/rullo/index.html"
+  },
+  {
+    "revision": "70ddc5c3b5bc1d8df2ee",
+    "url": "/rullo/static/css/main.2a6be18f.chunk.css"
+  },
+  {
+    "revision": "2dcabe5d98ceef36e6f9",
+    "url": "/rullo/static/js/2.a91aa3d0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/rullo/static/js/2.a91aa3d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ddc5c3b5bc1d8df2ee",
+    "url": "/rullo/static/js/main.1526f811.chunk.js"
+  },
+  {
+    "revision": "4af2fe811398b4533cee",
+    "url": "/rullo/static/js/runtime-main.5937024e.js"
+  }
+]);
